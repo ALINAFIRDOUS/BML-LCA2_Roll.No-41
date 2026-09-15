b0f7778d86bd7ebbd88a588a0bc3fdea29f74ec8
Alina firdous Abdulkadar Hunchalkar <alinafirdous2008@gmail.com>
2026-09-15 16:21:17 +0530

    Add files via upload

---
+++ knn_adult.py
@@ -0,0 +1,54 @@
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import LabelEncoder, StandardScaler
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.metrics import accuracy_score
+
+columns = [
+    "age", "workclass", "fnlwgt", "education", "education-num",
+    "marital-status", "occupation", "relationship", "race", "sex",
+    "capital-gain", "capital-loss", "hours-per-week",
+    "native-country", "income"
+]
+
+data = pd.read_csv(
+    "adult.data",
+    names=columns,
+    skipinitialspace=True
+)
+
+data.replace("?", pd.NA, inplace=True)
+data.dropna(inplace=True)
+
+categorical_cols = data.select_dtypes(
+    include=["object", "string"]
+).columns
+
+for col in categorical_cols:
+    le = LabelEncoder()
+    data[col] = le.fit_transform(data[col])
+
+X = data.drop("income", axis=1)
+y = data["income"]
+
+X_train, X_test, y_train, y_test = train_test_split(
+    X,
+    y,
+    test_size=0.20,
+    random_state=42
+)
+
+scaler = StandardScaler()
+
+X_train = scaler.fit_transform(X_train)
+X_test = scaler.transform(X_test)
+
+knn = KNeighborsClassifier(n_neighbors=5)
+
+knn.fit(X_train, y_train)
+
+y_pred = knn.predict(X_test)
+
+accuracy = accuracy_score(y_test, y_pred)
+
+print("KNN Accuracy:", round(accuracy * 100, 2), "%")
